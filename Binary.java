9bfb225ef211c5d46dc3b868616ddddd3bba0695
John Imeobong <imeobongjohn38@gmail.com>
2025-04-25 13:53:07 +0100

    More tasks

--- Binary.java
+++ Binary.java
@@ -4,28 +4,21 @@ public class Binary {
 	public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
 
-	
+	String binary = " ";
 
 	System.out.print("Enter a number: ");
 	int num = input.nextInt();
 
+	if (num = 0) {
+	binary = "0";
 
-	int num1 = num / 2;
-	int numb1 = (num / 2) % 2;
+}	else {
+	while (num % 2 = )
 
-	int num2 = num1 / 2;
-	int numb2 = (num1 / 2) % 2;
+}
 
-	int num3 = num2 / 2;
-	int numb3 = (num2 / 2) % 2;
 
-	int num4 = num3 / 1;
-	int numb4 = (num3 / 1) % 1;
-
-	System.out.print(numb1);
-	System.out.print(numb2);
-	System.out.print(numb3);
-	System.out.print(numb4);
+	
 }
 
 }
---
+++ HighestScoreII.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class HighestScoreII {
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+
+	String name = " ";
+	int count = 1;
+	int grade = 0;
+	int largest = 0;
+	int secLargest = 0;
+
+	System.out.print("Enter number of Students: ");
+	int num = input.nextInt();
+
+	while (count <= num) {
+	System.out.print("Enter Student's name: ");
+	name = input.next();
+
+	System.out.println("Welcome " + name);	
+
+	System.out.println("\nEnter Student grade: ");
+	grade = input.nextInt();
+
+	if (grade > largest) {
+	largest = grade;
+
+}	else if (grade != largest) {
+	largest = secLargest;
+	secLargest = grade;
+	name +=;
+}
+
+	count++;
+}
+	System.out.println(name + " has the highest grade.");
+	System.out.print("The second highest grade is " + secLargest);
+
+}
+
+
+
+}
